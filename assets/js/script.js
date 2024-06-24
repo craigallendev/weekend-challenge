6809302dfd3bc31596d13ade893c61bf3941427f
Craig Allen <callen7@hotmail.co.uk>
2024-06-24 10:58:21 +0000

    added html css and javascript

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const dob = document.getElementById('dob').value;
+    const location = document.getElementById('location').value;
+    const email = document.getElementById('email').value;
+
+    const output = `
+        <strong>Name:</strong> ${name}<br>
+        <strong>Date of Birth:</strong> ${dob}<br>
+        <strong>Location:</strong> ${location}<br>
+        <strong>Email Address:</strong> ${email}
+    `;
+
+    document.getElementById('output').innerHTML = output;
+});
